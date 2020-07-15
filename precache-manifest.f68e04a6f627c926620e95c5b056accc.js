a4c2d88d8da4dd2b45f04e29956ea412b083ee05
lewisjackson398 <lewisjackson398@gmail.com>
2020-07-16 00:02:42 +0100

    Updates

---
+++ precache-manifest.f68e04a6f627c926620e95c5b056accc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bef0fa3a69b4d4253e2986e6936af6a",
+    "url": "/lewis/index.html"
+  },
+  {
+    "revision": "fc80714a1c52050e3474",
+    "url": "/lewis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/lewis/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lewis/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc80714a1c52050e3474",
+    "url": "/lewis/static/js/main.730ae4f5.chunk.js"
+  },
+  {
+    "revision": "87ba207e395364d8eecf",
+    "url": "/lewis/static/js/runtime-main.dd6ff8d1.js"
+  }
+]);
